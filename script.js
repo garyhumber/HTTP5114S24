8771ebd87603b822a4662130a6b32a6672c952ac
Gaurav Bhanot <info@pixelr.io>
2024-06-20 11:25:50 -0400

    Adding the starter code

---
+++ script.js
@@ -0,0 +1,45 @@
+let currentInput = '0'; // This variable stores the current input in the display
+let previousInput = ''; // This variable stores the previous input for calculations
+let operator = null; // This variable stores the current operator
+
+// Function to update the display
+function updateDisplay() {
+    
+}
+
+// Function to clear the display and reset variables
+function clearDisplay() {
+    
+
+    updateDisplay();
+}
+
+// Function to append a number to the current input
+function appendNumber(number) {
+    
+    updateDisplay();
+}
+
+// Function to append an operator
+function appendOperator(op) {
+    
+    updateDisplay();
+}
+
+// Function to toggle the sign of the current input
+function toggleSign() {
+    
+    updateDisplay();
+}
+
+// Function to calculate percentage
+function percent() {
+    
+    updateDisplay();
+}
+
+// Function to perform the calculation based on the operator
+function calculate() {
+    
+    updateDisplay();
+}
